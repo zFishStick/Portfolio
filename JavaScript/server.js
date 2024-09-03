cd31a6bf30de29274aac8887000039278c4a931d
zFishStick <gabri.pesce0@gmail.com>
2024-09-03 16:25:41 +0200

    a

--- JavaScript/server.js
+++ JavaScript/server.js
@@ -2,33 +2,29 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const path = require('path');
 const AWS = require('aws-sdk');
-const fs = require('fs');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-const url = 'mongodb://localhost:27017'; // Cambia se usi un'URL diversa
+const url = process.env.MONGODB_URI; // Assicurati di configurare questa variabile d'ambiente su Vercel
 const dbName = 'Projects';
 
 // Configura le credenziali AWS
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,  // Usa variabili d'ambiente per maggiore sicurezza
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,  
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: 'eu-west-1' // Regione del tuo bucket S3
 });
 
 const s3 = new AWS.S3();
-const bucketName = 'pesca-games-images'; // Nome del tuo bucket S3
-
+const bucketName = 'pesca-games-images'; 
 
 app.use(express.static(path.join(__dirname, '../')));
 
-// Rotta per la root per servire 'index.html'
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-// Endpoint creato automaticamente
 app.get('/api/projects', async (req, res) => {
     const client = new MongoClient(url, { useUnifiedTopology: true });
 
