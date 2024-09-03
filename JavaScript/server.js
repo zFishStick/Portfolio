d2a0c9efa77974415ac35473d5163bb0e0dcf0e3
zFishStick <gabri.pesce0@gmail.com>
2024-09-03 18:55:50 +0200

    link di merda

--- JavaScript/server.js
+++ JavaScript/server.js
@@ -19,10 +19,9 @@ AWS.config.update({
 const s3 = new AWS.S3();
 const bucketName = 'pesca-games-images';  */
 
-app.use(express.static(path.join(__dirname, '../')));
 app.use('/Css', express.static(path.join(__dirname, '../Css')));
 app.use('/Html', express.static(path.join(__dirname, '../Html')));
-app.use('/JavaScript', express.static(path.join(__dirname, '../JavaScript')));
+app.use('/JavaScript', express.static(path.join(__dirname, 'JavaScript')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
